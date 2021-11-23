0ca2ef8cdb8e918e519bced5c4728ce6a3c5720e
Andrey Zuenkov <sadstatue.socnet@gmail.com>
2021-11-23 17:31:32 +0300

    create multimodules project

---
+++ module2/src/main/java/TestMultiModules.java
@@ -0,0 +1,11 @@
+public class TestMultiModules {
+    public static void main(String[] args) {
+        Employee employee = new Employee();
+        employee.setName("Сергей");
+        employee.setLastname("Петров");
+        employee.setAge(25);
+
+        System.out.println(employee);
+
+    }
+}
